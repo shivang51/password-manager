bde6954d946918893665e7eb67c32c58631fe8c5
shivang51 <aad9805066254@gmail.com>
2021-09-12 16:16:02 +0530

    Heroku Build

--- src/globals.ts
+++ src/globals.ts
@@ -19,11 +19,11 @@ interface IEnvVaribles {
 console.log(process.env.GMAIL);
 
 const variables = {
-  PORT: 8080 || result.parsed.PORT,
-  FIREBASE_ADMIN_KEY: process.env.FIREBASE_ADMIN_KEY || result.parsed.FIREBASE_ADMIN_KEY,
-  GMAIL: process.env.GMAIL || result.parsed.GMAIL,
-  GMAIL_PASSWORD : process.env.GMAIL_PASSWORD || result.parsed.GMAIL_PASSWORD,
+  PORT: 8080 || result.parsed?.PORT,
+  FIREBASE_ADMIN_KEY:
+    process.env.FIREBASE_ADMIN_KEY || result.parsed?.FIREBASE_ADMIN_KEY,
+  GMAIL: process.env.GMAIL || result.parsed?.GMAIL,
+  GMAIL_PASSWORD: process.env.GMAIL_PASSWORD || result.parsed?.GMAIL_PASSWORD,
 };
 
-export const env_variables: IEnvVaribles =
-  variables as unknown as IEnvVaribles;
+export const env_variables: IEnvVaribles = variables as unknown as IEnvVaribles;
