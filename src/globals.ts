6aa3731419892554f50b31871db06a1051d8ebc7
shivang51 <aad9805066254@gmail.com>
2021-09-12 16:05:12 +0530

    Heroku Build

--- src/globals.ts
+++ src/globals.ts
@@ -16,5 +16,14 @@ interface IEnvVaribles {
   GMAIL_PASSWORD: string;
 }
 
+console.log(process.env.GMAIL);
+
+
+if(result.parsed){
 export const env_variables: IEnvVaribles =
   result.parsed as unknown as IEnvVaribles;
+}
+else{
+export const env_variables: IEnvVaribles =
+  {PORT: 8080, FIREBASE_ADMIN_KEY: process.env.FIREBASE_ADMIN_KEY, GMAIL: process.env.GMAIL, GMAIL_PASSWORD : process.env.GMAIL_PASSWORD} as unknown as IEnvVaribles;
+}
